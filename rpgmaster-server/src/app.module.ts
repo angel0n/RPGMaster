9abcc48f233eed782c92deeb4b1d0c83e9abfa34
angel0n <63112226+angel0n@users.noreply.github.com>
2025-09-02 10:56:35 -0300

    Configuração do typeORM

--- rpgmaster-server/src/app.module.ts
+++ rpgmaster-server/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD?.toString(),
+      database: process.env.TYPEORM_DATABASE,
+      synchronize: false,
+    })
+  ],
   controllers: [],
   providers: [],
 })
